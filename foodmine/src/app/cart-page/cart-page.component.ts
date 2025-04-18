fd8ddcc6c292bf8fe6d433a7b5628c9abcebfba3
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2025-04-18 21:28:02 +0530

    updated code

--- foodmine/src/app/cart-page/cart-page.component.ts
+++ foodmine/src/app/cart-page/cart-page.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart/cart.service';
 import { Cart } from '../shared/models/Cart';
 import { CartItem } from '../shared/models/CardItem';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
 @Component({
   selector: 'app-cart-page',
-  imports: [NgFor, RouterModule, CommonModule],
+  imports: [NgFor, NgIf, RouterModule, CommonModule, NotFoundComponent],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.css'
 })
@@ -19,7 +20,7 @@ export class CartPageComponent implements OnInit{
     this.setCart();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  
       
   }
 
